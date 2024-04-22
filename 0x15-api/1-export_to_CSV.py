bc2e78e9b8ba074681658f5f4272b1e2db18161b
Ayokayzy <theayokayzy1@gmail.com>
2024-04-22 23:32:16 +0100

    Export to CSV

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
         f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
     )
     tasks = todos.json()
-Export to CSV
+
     with open(f'{employee_id}.csv', 'w') as file:
         writer = csv.writer(file, dialect="unix", delimiter=",")
         for task in tasks:
